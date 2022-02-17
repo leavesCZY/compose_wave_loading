135787803483e1daf30689cf612dad710bf36998
leavesCZY <1990724437@qq.com>
2022-02-17 10:57:02 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ComposeWaveLoading
+
+Jetpack Compose 与 贝塞尔曲线 的结合使用
+
+![](https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/7b6aaf38932043ea8cf54f2402b5e3fa~tplv-k3u1fbpfcp-zoom-1.image)
---
+++ app/src/main/java/github/leavesczy/waveloading/WaveLoading.kt
@@ -0,0 +1,119 @@
+package github.leavesczy.waveloading
+
+import android.graphics.Typeface
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.TextUnit
+
+/**
+ * @Author: leavesCZY
+ * @Date: 2022/2/16 15:39
+ * @Github: https://github.com/leavesCZY
+ * @Desc:
+ */
+@Composable
+fun WaveLoading(
+    modifier: Modifier,
+    text: String,
+    textSize: TextUnit,
+    waveColor: Color,
+    downTextColor: Color = Color.White,
+    animationSpec: InfiniteRepeatableSpec<Float> = infiniteRepeatable(
+        animation = tween(
+            durationMillis = 500,
+            easing = CubicBezierEasing(0.2f, 0.2f, 0.7f, 0.9f)
+        ),
+        repeatMode = RepeatMode.Restart
+    )
+) {
+    BoxWithConstraints(modifier = modifier) {
+        val density = LocalDensity.current.density
+        val circleSizeDp = minOf(maxWidth, maxHeight)
+        val circleSizePx = circleSizeDp.value * density
+        val waveWidth = circleSizePx / 1.2f
+        val waveHeight = circleSizePx / 10f
+        val textPaint by remember {
+            mutableStateOf(Paint().asFrameworkPaint().apply {
+                isAntiAlias = true
+                isDither = true
+                typeface = Typeface.create(Typeface.SANS_SERIF, Typeface.BOLD)
+                textAlign = android.graphics.Paint.Align.CENTER
+            })
+        }
+        val wavePath by remember {
+            mutableStateOf(Path())
+        }
+        val circlePath by remember {
+            mutableStateOf(Path().apply {
+                addArc(
+                    Rect(0f, 0f, circleSizePx, circleSizePx),
+                    0f, 360f
+                )
+            })
+        }
+        val animateValue by rememberInfiniteTransition().animateFloat(
+            initialValue = 0f,
+            targetValue = waveWidth,
+            animationSpec = animationSpec,
+        )
+        Canvas(modifier = modifier.requiredSize(size = circleSizeDp)) {
+            drawTextToCenter(
+                text = text,
+                textPaint = textPaint,
+                textSize = textSize.toPx(),
+                textColor = waveColor.toArgb()
+            )
+            wavePath.reset()
+            wavePath.moveTo(-waveWidth + animateValue, circleSizePx / 2.3f)
+            var i = -waveWidth
+            while (i < circleSizePx + waveWidth) {
+                wavePath.relativeQuadraticBezierTo(waveWidth / 4f, -waveHeight, waveWidth / 2f, 0f)
+                wavePath.relativeQuadraticBezierTo(waveWidth / 4f, waveHeight, waveWidth / 2f, 0f)
+                i += waveWidth
+            }
+            wavePath.lineTo(circleSizePx, circleSizePx)
+            wavePath.lineTo(0f, circleSizePx)
+            wavePath.close()
+            val resultPath = Path.combine(
+                path1 = circlePath,
+                path2 = wavePath,
+                operation = PathOperation.Intersect
+            )
+            drawPath(path = resultPath, color = waveColor)
+            clipPath(path = resultPath, clipOp = ClipOp.Intersect) {
+                drawTextToCenter(
+                    text = text,
+                    textPaint = textPaint,
+                    textSize = textSize.toPx(),
+                    textColor = downTextColor.toArgb()
+                )
+            }
+        }
+    }
+}
+
+private fun DrawScope.drawTextToCenter(
+    text: String,
+    textPaint: android.graphics.Paint,
+    textSize: Float,
+    textColor: Int
+) {
+    textPaint.textSize = textSize
+    textPaint.color = textColor
+    val fontMetrics = textPaint.fontMetrics
+    val x = size.width / 2f
+    val y = size.height / 2f - (fontMetrics.top + fontMetrics.bottom) / 2f
+    drawContext.canvas.nativeCanvas.drawText(text, x, y, textPaint)
+}
